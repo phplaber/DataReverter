2e77360d6a8f2496daaa685e9fc669bba2514e64
Yns0ng <1004609616@qq.com>
2016-05-05 20:00:58 +0800

    just a commit

---
+++ DataReverter.php
@@ -0,0 +1,147 @@
+<?php
+// 数据回滚组件
+
+class DataReverter extends CApplicationComponent
+{
+    // 默认DB组件名称
+    public $defaultDb = 'db';
+    // 默认缓存生命周期
+    public $defaultExpire = 10800;
+    private $_cache = null;
+    private $_table = '';
+    private $_data  = array();
+    private $_where = '';
+    private $_pk = '';
+    private $_fieldList = array();
+    private $_range = array();
+
+    public function init()
+    {
+        parent::init();
+        if ($this->_cache === null)
+            $this->_cache = Yii::app()->memcache;
+    }
+
+    public function setTable($table = '')
+    {
+        if (empty($table))
+            throw new CException('The table cannot be empty');
+
+        $this->_table = $table;
+
+        return $this;
+    }
+
+    public function setWhere($where = '')
+    {
+        if (empty($where))
+            throw new CException('The condition cannot be empty');
+
+        $this->_where = $where;
+
+        return $this;
+    }
+
+    public function setField($fieldList = array())
+    {
+        if (empty($fieldList))
+            throw new CException('The fields cannot be empty');
+
+        // 增加主键字段
+        !in_array($this->_pk, $fieldList) && array_unshift($fieldList, $this->_pk);
+        $this->_fieldList = $fieldList;
+
+        return $this;
+    }
+
+    public function setPk()
+    {
+        // 获取主键
+        $queryKeySql = <<<QUERY
+    SHOW KEYS FROM {$this->_table} WHERE Key_name = 'PRIMARY';
+QUERY;
+        $row = Yii::app()->{$this->defaultDb}->createCommand($queryKeySql)->queryRow();
+        $this->_pk = $row['Column_name'];
+
+        return $this;
+    }
+
+    public function setPkRange($range = '')
+    {
+        if (empty($range) || strpos($range, '-') === false)
+            throw new CException('The pk range cannot be empty or invalid format');
+
+        $this->_range = explode('-', $range);
+
+        return $this;
+    }
+
+    public function query()
+    {
+        $fields = implode(',', $this->_fieldList);
+
+        $sql = <<<QUERY
+    select {$fields} from {$this->_table} where {$this->_where};
+QUERY;
+        $ret = Yii::app()->{$this->defaultDb}->createCommand($sql)->queryAll();
+        if ($ret) {
+            $this->_data = $ret;
+        }
+
+        return $this;
+    }
+
+    public function store()
+    {
+        if (!$this->_cache->set($this->getKey(), $this->_data, $this->defaultExpire))
+            throw new CException('Memcache must be required');
+    }
+
+    public function revert()
+    {
+        $transaction = Yii::app()->{$this->defaultDb}->beginTransaction();
+        $isException = false;
+
+        $pk = $this->_pk;
+        if (!empty($this->_where)) {
+            $data = $this->_cache->get($this->getKey());
+            if ($data === false)
+                throw new CException('Revert failed');
+
+            foreach ($data as $item) {
+                $update = "update {$this->_table} set ";
+                foreach ($item as $field => $value) {
+                    if ($field == $pk) continue;
+                    $update .= $field . '=' . (is_int($value) ? $value : "'{$value}'") . ',';
+                }
+                $update = rtrim($update, ',');
+                $update .= " where {$pk} = $item[$pk]; ";
+
+                try {
+                    Yii::app()->{$this->defaultDb}->createCommand($update)->execute();
+                } catch (CException $e) {
+                    $isException = true;
+                }
+            }
+        } elseif (!empty($this->_range)) {
+            // 根据主键ID范围去删除
+            $start = intval($this->_range[0]);
+            $end = intval($this->_range[1]);
+            $delete = "delete from {$this->_table} where {$pk} >= {$start} and {$pk} <= {$end}; ";
+
+            try {
+                Yii::app()->{$this->defaultDb}->createCommand($delete)->execute();
+            } catch (CException $e) {
+                $isException = true;
+            }
+        } else {}
+
+        // 提交或回滚
+        !$isException ? $transaction->commit() : $transaction->rollback();
+    }
+
+    protected function getKey()
+    {
+        return md5(__CLASS__ . $this->defaultDb . $this->_table . $this->_where);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,136 @@
 # DataReverter
-A yii component for revert changed db data.
+一个用于回滚已变更DB数据的Yii的组件
+
+#### 目的
+回滚那些需要执行完SQL后人肉去检查或执行SQL之后才能知道是否正确的数据。
+
+#### 目标
+1.  Yii风格组件形式，继承CApplicationComponent类；
+2.  灵活，支持多张表&多个字段数据回滚；
+3.  OOP风格，封装良好，便于使用；
+4.  健壮可靠；
+5.  提供Web配置界面。
+
+#### 优缺点
+
+##### 优点
+1.  更新或删除操作完全基于主键；
+2.  严格检测必填字段；
+3.  回滚时使用事务，确保数据一致性；
+4.  提供Web配置界面，操作方便；
+5.  部署简单。
+
+##### 缺点
+1.  只支持单表数据回滚；
+2.  依赖memcache，如果缓存数据丢失，则数据不可回滚。
+
+#### 组成
+1.  UI - 提供Web界面用于配置（回滚方式(更新or删除) + 更新：表名，查询条件，字段；删除：表名，主键范围）；
+2.  Ajax - 异步请求；
+3.  Controller - 接受请求，调用组件方法处理请求；
+4.  Component - 封装业务逻辑；
+5.  Config - 注册和初始化组件；
+6.  Memcache - 储存原始数据。
+
+#### 控制器代码片段
+
+*  储存数据Action
+
+```php
+<?php
+
+public function actionStore()
+{
+    $response = $this->_packResponse(405, 'Method not allowed');
+    if (Yii::app()->request->getIsAjaxRequest()) {
+        $table = Yii::app()->request->getPost('table');
+        $fieldList = array_filter(explode(',', Yii::app()->request->getPost('field')));
+        $where = Yii::app()->request->getPost('where');
+        try {
+            Yii::app()->revert
+                ->setTable($table)
+                ->setWhere($where)
+                ->setPk()
+                ->setField($fieldList)
+                ->query()
+                ->store();
+            $response = $this->_packResponse();
+        } catch (CException $e) {
+            $response = $this->_packResponse(500, $e->getMessage());
+        }
+    }
+
+    @header('Content-type: application/json');
+    exit(json_encode($response));
+}
+```
+
+*  回滚数据Action
+
+```php
+<?php
+
+public function actionRevert()
+    {
+        $response = $this->_packResponse(405, 'Method not allowed');
+        if (Yii::app()->request->getIsAjaxRequest()) {
+            $table = Yii::app()->request->getPost('table');
+            $type = Yii::app()->request->getPost('type');
+            $where = Yii::app()->request->getPost('where');
+            try {
+                if ($type == 'update') {
+                    Yii::app()->revert
+                        ->setTable($table)
+                        ->setWhere($where)
+                        ->setPk()
+                        ->revert();
+                } elseif ($type == 'delete') {
+                    Yii::app()->revert
+                        ->setTable($table)
+                        ->setPk()
+                        ->setPkRange($where)
+                        ->revert();
+                } else {
+                    // todo
+                }
+                $response = $this->_packResponse();
+            } catch (CException $e) {
+                $response = $this->_packResponse(500, $e->getMessage());
+            }
+        }
+
+        @header('Content-type: application/json');
+        exit(json_encode($response));
+    }
+```
+
+*  封装响应数据方法
+
+```php
+<?php
+
+private function _packResponse($code = 200, $msg = 'OK')
+    {
+        return array(
+            'code' => $code,
+            'message' => $msg
+        );
+    }
+```
+
+#### 部署
+部署Yii组件的方法，在配置文件里引入一个组件即可。
+
+```php
+<?php
+// ...
+'components' => array(
+        'revert' => array(
+            'class' => 'DataReverter',
+        ),
+    ),
+// ...
+```
+
+#### UI
+![alt DataReverter](https://raw.githubusercontent.com/phplaber/DataReverter/master/DataReverter.png)
